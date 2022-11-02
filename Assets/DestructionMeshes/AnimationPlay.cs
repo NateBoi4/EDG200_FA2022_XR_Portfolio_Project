3731c657a6b7965a0f4c648b02ffaee5c8787c79
Henry Foley <henry.foley@mymail.champlain.edu>
2022-11-01 21:58:50 -0400

    Added in example destruction
    * Meshes and Animations imported
    * Play animation on hit
    * Ability to switch between animations

---
+++ Assets/DestructionMeshes/AnimationPlay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animation anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim.Play("Back");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
